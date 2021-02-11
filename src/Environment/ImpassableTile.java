4b6a420de58af9d5738c9f02236e25d9422e7438
Velin Andreev <63392930+Velin-A@users.noreply.github.com>
2021-02-11 18:30:25 +0200

    third commit

---
+++ src/Environment/ImpassableTile.java
@@ -0,0 +1,14 @@
+package Environment;
+
+import Environment.Environment;
+
+import java.awt.*;
+
+public class ImpassableTile extends Environment {
+
+        public ImpassableTile (int row, int col, Color color){
+            this.row   = row;
+            this.col   = col;
+            this.color = color;
+        }
+}
